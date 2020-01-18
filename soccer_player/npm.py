9703f26dedcc16398f5dea0ef058a666e918690a
Flouis2017 <315587951@qq.com>
2020-01-18 16:14:34 +0800

    20200118 day

--- soccer_player/npm.py
+++ soccer_player/npm.py
@@ -67,7 +67,7 @@ plt.show()
 """
 
 """
-# 统计拥有忠心（五年及以上）球员的前十个俱乐部
+# 统计拥有忠心（五年及以上）球员的前十个俱乐部 —— 意义：查看哪些俱乐部最能留住球员
 year = data['Club_Joining'].apply(lambda x: x.split('/')[-1]).astype(np.int)
 # print(year)
 # 用获取数据集年份 和 年份数据 做差，筛选出五年以上且不是“自由身”的球员
@@ -79,35 +79,42 @@ p = g.plot(kind='bar')
 plt.show()
 """
 
+"""
+# 某一列裂项——这里以生日为例，将出生年月裂项为年月日
+t = data['Birth_Date'].str.split("/", expand=True)
+# print(t)
+# t[0].value_counts().plot(kind='bar')
+# value_counts()和groupby().size()异同：首先它们都可以用于分组求和
+# value_counts()只能用在Series，并且对结果使用列降序排列
+# groupby().size()可以用在Series和DataFrame，对结果进行行升序排列
+t = t.groupby(0).size().sort_values(ascending=False).plot(kind='bar')
 
+plt.show()
+"""
 
+"""
+# 足球运动员号码和位置的关系
+# 过滤替补球员和预备队球员
+t = data[(data['Club_Position'] != 'Sub') & (data['Club_Position'] != 'Res')]
+t = t.groupby(['Club_Kit', 'Club_Position']).size().sort_values(ascending=False)
+print(t)
+"""
 
+"""
+# 年龄和评分关系——散点图
+data.plot(kind='scatter', x='Age', y='Rating')
+plt.show()
+"""
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+"""
+# 年龄段和评分关系——折线图
+"""
+t = data.copy()
+t['Age_Section'] = pd.cut(t['Age'], bins=[1, 20, 30, 40, 100], labels=["<20", "20-30", "30-40", ">40"])
+t = t.groupby('Age_Section')['Rating'].mean()
+print(t)
+t.plot(marker='o', xticks=[0, 1, 2, 3])
+plt.show()
 
 
 
--- test/my_test.py
+++ test/my_test.py
@@ -8,6 +8,8 @@ xc = "%s %s" % ("hello", 2019)
 print(type(xc))
 
 a = "173 cm"
-a = a[0:-3]
+print(a[0:-3])
+a = "07/14/2020"
+print(a[-4:])
 print(int("123 "))
 
